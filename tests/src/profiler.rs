3e83566d19bcdc95fb4ea61e7f7e03df4f1e979c
M. Daeva <embedmad@gmail.com>
2025-09-04 18:35:04 +0300

    updated profiling results

--- tests/src/profiler.rs
+++ tests/src/profiler.rs
@@ -45,11 +45,11 @@ fn profiling_registry() -> TestResult<()> {
     const PROGRAM_NAME: &str = PROGRAM_NAME_REGISTRY;
     const PREVIOUS_RESULT: &str = "
 ==================== previous result ====================
-registry program size: 72824 B
-registry init instruction: 36958 CU
-registry create_account instruction: 19466 CU
-registry activate_account instruction: 14548 CU
-registry write_data instruction: 5293 CU
+registry program size: 49240 B
+registry init instruction: 36700 CU
+registry create_account instruction: 19114 CU
+registry activate_account instruction: 14222 CU
+registry write_data instruction: 4785 CU
 ==================== current result =====================";
 
     println!("{}", PREVIOUS_RESULT);
